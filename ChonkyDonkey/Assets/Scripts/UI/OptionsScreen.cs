c8c66a96246033bd1b68ed3514cd00b39f1e8b4a
cjuillard <colin.juillard@gmail.com>
2022-07-16 13:52:56 -0600

    Integrate the main menu + options art.

--- ChonkyDonkey/Assets/Scripts/UI/OptionsScreen.cs
+++ ChonkyDonkey/Assets/Scripts/UI/OptionsScreen.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsScreen : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public Toggle soundToggle;
+
+    public void OnAwake()
+    {
+        // TODO sync these values to some global settings object
+        volumeSlider.SetValueWithoutNotify(0.5f);
+        soundToggle.SetIsOnWithoutNotify(true);
+    }
+
     public void OnBackButtonClicked() 
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnSliderChanged() 
+    {
+        Debug.Log($"OnSliderChanged: {volumeSlider.value}");
+    }
+    
+    public void OnSoundToggleChanged(bool value)
+    {
+        Debug.Log($"OnSoundToggleChanged: {value}");
     }
 }
